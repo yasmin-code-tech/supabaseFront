1e50c9b80a3f9ffa1ba269871259563b13680256
yasmin-code-tech <yasmin.jobs33@gmail.com>
2024-11-06 20:35:26 -0300

    Update app.js coloquei o IP

--- supabasefront/app.js
+++ supabasefront/app.js
@@ -54,7 +54,7 @@ addProductForm.addEventListener('submit', async event => {
 
 // Function to add a new product
 async function addProduct(name, price) {
-  const response = await fetch('http://localhost:3000/products', {
+  const response = await fetch('http:// 3.14.150.40:3000/products', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -66,7 +66,7 @@ async function addProduct(name, price) {
 
 // Function to delete a new product
 async function deleteProduct(id) {
-  const response = await fetch('http://localhost:3000/products/' + id, {
+  const response = await fetch('http:// 3.14.150.40:3000/products/' + id, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json'
